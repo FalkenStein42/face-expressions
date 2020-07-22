7555a1a9fb5d161a4d736d3fa818527578fbc496
Sergio Solé <sergio.sole99@gmail.com>
2020-07-22 23:42:00 +0200

    added axis parameter to findClosest()

--- point_operations.py
+++ point_operations.py
@@ -5,7 +5,6 @@ The purpose of this module is to provide tools for computing poitn cluster dista
 These allow for the identification and prediction of the positions of reference points on the model.
 
 TODO:
--implement trimmed median algorithm on findCentralPoint()
 -generalize findClosest() to allow for specific axis and axis directions. Potentially separate this into a
     diferent version of the function
 -implement an identification function for a set of points based on a fixed model
@@ -29,15 +28,23 @@ def joinClosest(frame, points, connections = 2):
     pass
  
             
-def findClosest(points, point):
+def findClosest(points, point, axis=-1):
+    '''
+    axis 0 -> X
+    axis 1 -> Y
+    '''
     points = points-point #Shift origin to target
     points = np.delete(points, \
               np.where(np.all(np.equal(points,np.array([[0,0]])),axis=1)),\
               axis=0) #Delete itself from the list (now transformed to [0,0]) if it exists
 
-    norm = np.linalg.norm(points,axis=1) #Make the vector norm array
+    dist = np.linalg.norm(points,axis=1) #Make the vector norm array
+    if(axis==-1): #Check wether an axis was specified
+        rating = dist
+    else:         #Assign rating as a weighted sum of distance and axis coordinate 
+        rating = dist + abs(((points[:,axis])**2)/np.amax(points[:,axis]))
     
-    return (points[np.where(norm == np.amin(norm))] + point) #Return elements wich correspond with the index of the
+    return (points[np.where(rating == np.amin(rating))] + point) #Return elements wich correspond with the index of the
                                                              # lowest norm, re-normalized to global coordinates
     
 def findCentralPoint(points):
@@ -68,9 +75,6 @@ def findCentralPoint(points):
 
     return points[best_i]
             
-
-
-
 
 
                 
